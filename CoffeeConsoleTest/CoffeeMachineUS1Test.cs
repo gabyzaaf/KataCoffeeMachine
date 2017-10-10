513b3e9de4e616773f0936e4fd1101f067d6928f
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-10 13:49:14 +0200

    [US1] - Add Coffee class

--- CoffeeConsoleTest/CoffeeMachineUS1Test.cs
+++ CoffeeConsoleTest/CoffeeMachineUS1Test.cs
@@ -13,22 +13,63 @@ namespace CoffeeConsoleTest
     {
      
         [TestCase("Tea")]
-        public void Choose_Tea_Without_Sugar_And_Stick(string teaChoosen)
+        public void Choose_Tea_Without_Sugar_And_Stick_And_Will_Return_The_Machine_Format(string teaChoosen)
         {
             if ("Tea".Equals(teaChoosen) == false)
             {
-                throw new Exception("The choose is not a tea");
+                throw new Exception("The choose is not a Tea");
             }
 
             Tea tea = new Tea();
-            Check.That(tea.MachineEncoding).Equals("T");
-            
+            Check.That(tea.MachineEncoding).Equals("T:"); 
+        }
+
+        [TestCase("Chocolate")]
+        public void Choose_Chocolate_Without_Sugar_And_Stick_And_Will_Return_The_Machine_Format(string chocolatChoosen)
+        {
+            if ("Chocolate".Equals(chocolatChoosen) == false)
+            {
+                throw new Exception("The choose is not a chocolate");
+            }
+
+            Chocolate chocolate = new Chocolate();
+            Check.That(chocolate.MachineEncoding).Equals("H:");
+        }
+
+        [TestCase("Coffee")]
+        public void Choose_Coffee_Without_Sugar_And_Stick_And_Will_Return_The_Machine_Format(string coffeeChoosen)
+        {
+            if ("Coffee".Equals(coffeeChoosen) == false)
+            {
+                throw new Exception("The choose is not a coffee");
+            }
+            Coffee coffee = new Coffee();
+            Check.That(coffee.MachineEncoding).Equals("C:");
+        }
+
+        
+    }
+
+    internal class Coffee
+    {
+        public string MachineEncoding { get; private set; } = "C:";
+
+
+    }
+
+    internal class Chocolate
+    {
+        public string MachineEncoding { get; private set; } = "H:";
+
+        public Chocolate()
+        {
+
         }
     }
 
     public class Tea
     {
-        public string MachineEncoding { get; private set; } = "T";
+        public string MachineEncoding { get; private set; } = "T:";
 
         public string GetEncoding()
         {
