8e73f4fea567909a02eadecfba3e137bdc11c2e2
gabriel <gabriel.zaafrani@lcdlv.com>
2017-10-12 21:45:08 +0200

    [US1] - Add Choose coffee without sugar

--- CoffeeConsoleTest/CoffeeMachineUS1Test.cs
+++ CoffeeConsoleTest/CoffeeMachineUS1Test.cs
@@ -44,15 +44,102 @@ namespace CoffeeConsoleTest
                 throw new Exception("The choose is not a coffee");
             }
             Coffee coffee = new Coffee();
-            Check.That(coffee.MachineEncoding).Equals("C:");
+            Check.That(coffee.MachineEncoding).Equals("C");
         }
+
+
+        [Test]
+        public void AddSugar_Add_minimum_0_and_max_2()
+        {
+                Sugar sugar = new Sugar(2);
+                Check.That(sugar.IsValid()).IsTrue();
+        }
+
+        [Test]
+        public void DisplaySugar_Should_Display_The_Sugar_Number_If_The_Sugar_Number_is_Valid()
+        {
+            int sugarNumber = 2;
+            Sugar sugar = new Sugar(sugarNumber);
+            if (sugar.IsValid())
+            {
+                Check.That(sugar.NumberSugarForTheScreen()).IsEqualTo(":2");
+            }
+        }
+
+        [Test]
+        public void ChooseStick_Should_return_One_Stick()
+        {
+            Stick stick = new Stick();
+            stick.ObtainOne();
+            string stickAvailable = stick.Display();
+            Check.That(stickAvailable).IsEqualTo(":1");
+        }
+
+        [Test]
+        public void Choose_Coffee_Without_Sugar()
+        {
+            Coffee coffee = new Coffee();
+            Sugar sugar = new Sugar(0);
+            Stick stick = new Stick();
+            string machineFormat = $"{coffee.MachineEncoding}{sugar.NumberSugarForTheScreen()}{stick.Display()}";
+            Check.That(machineFormat).IsEqualTo("C:0:0");
+        }
+
+
+        
+    }
+
+    internal class Stick
+    {
+        private int numberStick;
+
+        public Stick()
+        {
+            
+        }
+
+        public void ObtainOne()
+        {
+            numberStick = 1;
+        }
+
+        internal string Display()
+        {
+            return $":{numberStick.ToString()}";
+        }
+    }
+
+    internal class Sugar
+    {
+        private int sugar;
+
+
+        public Sugar(int sugar)
+        {
+            this.sugar = sugar;
+        }
+
+        public bool IsValid()
+        {
+            if (this.sugar <0 || this.sugar > 2 )
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string NumberSugarForTheScreen()
+        {
+            return $":{this.sugar.ToString()}";
+        }
+
 
         
     }
 
     internal class Coffee
     {
-        public string MachineEncoding { get; private set; } = "C:";
+        public string MachineEncoding { get; private set; } = "C";
 
 
     }
